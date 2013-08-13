abfac6ba31a7a32ae4890c00414cff48597aad64
Christian Grimsgaard <chisse1049@hotmail.com>
2013-08-13 16:48:57 +0200

    WHALE

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var mouseX = 0, mouseY = 0;
 var speed = 2;
-var size = {x:69,y:74};
+var size = {x:224,y:242};
+var timer;
 
 $(document).mousemove(function(e){
 	mouseX = e.pageX;
@@ -22,26 +23,47 @@ for(var i = 0; i < $(".stalker").length; i++) {
 			"z-index": 100-i,
 			"width": size.x,
 			"height": size.y,
-			"background-image": 'url("img/' + (i+1) + '.png")',
+			"background-image": 'url("img/whale-' + (i+1) + '.png")',
 			"margin-left": (size.y / -2),
 			"margin-top": (size.x / -2)
 		}
 	)
 }
 
+function loop() {
+	setInterval(function(){
+		for(i in stalkers) {
+			if(i == 0) {
+				stalkers[i].x += (mouseX - stalkers[i].x) / speed;
+				stalkers[i].y += (mouseY - stalkers[i].y) / speed;
+			}
+			else {
+				stalkers[i].x += (stalkers[i-1].x - stalkers[i].x) / speed;
+				stalkers[i].y += (stalkers[i-1].y - stalkers[i].y) / speed;
+			}
+			
+			$(stalkers[i].el).css({left:stalkers[i].x, top:stalkers[i].y});
+		}
+	}, 30);
+}
 
-
-var loop = setInterval(function(){
-	for(i in stalkers) {
-		if(i == 0) {
+/*function loop() {
+	setInterval(function() {
+		var i = 0;
+		var interval = setInterval(function() {
 			stalkers[i].x += (mouseX - stalkers[i].x) / speed;
 			stalkers[i].y += (mouseY - stalkers[i].y) / speed;
-		}
-		else {
-			stalkers[i].x += (stalkers[i-1].x - stalkers[i].x) / speed;
-			stalkers[i].y += (stalkers[i-1].y - stalkers[i].y) / speed;
-		}
-		
-		$(stalkers[i].el).css({left:stalkers[i].x, top:stalkers[i].y});
-	}
-}, 30);
+			
+			$(stalkers[i].el).css({left:stalkers[i].x, top:stalkers[i].y});
+			
+			i++;
+			
+			if(i >= stalkers.length) {
+				clearInterval(interval);
+				i = 0;
+			}
+		}, 30);
+	}, 30);
+}*/
+
+loop();
